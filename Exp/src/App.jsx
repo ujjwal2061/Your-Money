520a533761c3ac76e33789f9fb1fcbd808857faa
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-11-29 21:09:23 +0530

    Expense Track

---
+++ Exp/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Selection from './Section/Slection'
+import Header from './Component/Header'
+import Content from './Component/Content'
+import Footer from './Component/Footer'
+export default function App() {
+  return (
+    <>
+    <Selection>
+      <Header />
+      <Content />
+      <Footer />
+    </Selection>
+
+    </>
+  )
+}
---
+++ Exp/src/Section/Slection.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const Selection=({children})=>{
+return (
+    <section className="relative  w-full md:w-[850px] h-[100vh]  mx-auto">
+            {children}
+        </section>
+)
+}
+export default Selection;
