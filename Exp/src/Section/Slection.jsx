6759e9ddabaf7a55af9503b19a9fbbdae950a78d
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-01 18:20:11 +0530

    Little Bit Need to Complete

--- Exp/src/Section/Slection.jsx
+++ Exp/src/Section/Slection.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const Selection=({children})=>{
 return (
-    <section className="relative  w-full md:w-[850px] h-full  mx-auto">
+    <section className="relative w-full md:w-[850px] h-[100vh]    mx-auto">
             {children}
         </section>
 )
