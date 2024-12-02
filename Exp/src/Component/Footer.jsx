597ea23f9c72365571de681d37adaa1cb950ab6d
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-02 17:01:53 +0530

    Almost Done

--- Exp/src/Component/Footer.jsx
+++ Exp/src/Component/Footer.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <section className=' bottom-12  right-1 left-2 p-5   flex-col justify-center md:flex-row'>
+       <div className='p-2  rounded-md  flex flex-col items-center gap-2 justify-center md:flow-row '>
+       <div className='px-7 py-3 flex flex-row justify-center gap-6 text-black rounded-md'>
+           <a href='https://x.com/Ujjwal_2061' target='_blank' ><i className="fa-brands fa-x-twitter"></i></a>
+           <a href='https://github.com/ujjwal2061' target='_blank' ><i className="fa-brands fa-github"></i></a>
+         </div>
+         <p className="  underline font-itim text-xl">I will add more Feature in this Project && Don't Forget to Follow me</p>
+         </div>
       
-    </div>
+    </section>
   )
 }
