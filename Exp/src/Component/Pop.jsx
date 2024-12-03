e28d9ec0cf7c79d6c36af8a7f3ee386be782b2f7
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-04 01:58:55 +0530

    Done

--- Exp/src/Component/Pop.jsx
+++ Exp/src/Component/Pop.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
+import React   from 'react'
 
-export default function Pop({value}) {
-   
+export default function Pop({value}) { 
   return (
-    <div>
-       <p>Value can't  be zero:{value} </p>
+    <div  className='p-2 rounded-md   bg-red-200  w-1/2 flex flex-col  justify-start md:fex-row '>
+       <span className='p-1 text-sm   font-serif font-semibold  text-black'>Value can't  be zero:{value} </span>
     </div>
   )
 }
