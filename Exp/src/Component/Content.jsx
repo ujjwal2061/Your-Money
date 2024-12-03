f47c56bb10dd1140e3fc24a3cc3ef898f92ae57f
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-03 22:21:10 +0530

    WTF i am doing

--- Exp/src/Component/Content.jsx
+++ Exp/src/Component/Content.jsx
@@ -2,25 +2,28 @@ import React, { useState } from 'react'
 import {useRef} from 'react'
 import Pop from './Pop';
 
-export default function Content({}) {
+export default function Content() {
   const spanRef = useRef(null);
   const [total ,setTotal]=useState(0);
   const [inputvalue,setInputvalue]=useState("")
   const [name,setName]=useState("");
   const [amount,setAmount]=useState("")
   const [allOver,setAllover]=useState([]);
-  const [NewTotalValue,setNewTotalValue]=useState("")
-  const [showPop, setShowPop] = useState(true); 
+  const [NewTotalValue,setNewTotalValue]=useState("") 
+  const [showPop, setShowPop] = useState(false); 
 
   const handlebtn=(event)=>{
     event.preventDefault()
     // pass the value to pop as a props 
-       setShowPop(false);
-       <Pop value={inputvalue} showPop={showPop} />
+      if(parseFloat(inputvalue)== 0){
+      setShowPop(true);
+      }else{
+        setTotal((total)=>total+parseFloat(inputvalue))
+        setInputvalue((prevTotal)=>prevTotal+parseFloat(inputvalue))
+        setInputvalue("")
+
+      }
       //-> the parseFloat is use to convert the inputvalue to the number 
-      setTotal((total)=>total+parseFloat(inputvalue))
-      setInputvalue((prevTotal)=>prevTotal+parseFloat(inputvalue))
-      setInputvalue("")
   }
 
   const handleraddbtn=(e)=>{
@@ -52,6 +55,7 @@ export default function Content({}) {
       <div className='flex flex-row justify-center  gap-2 p-2  rounded-lg '>
         <div className='flex flex-col  shadow-2xl gap-3 items-center justify-center text-black  px-8 py-4 rounded-md md:flex-row sm:flex-col'>
         <form onSubmit={handlebtn} >
+        {showPop && <Pop value={parseFloat(inputvalue)} />}
           <label className='p-1  font-semibold font-jetbrains'>Income</label>
           <input  className='m-2 border-2 transition ease-in-out delay-150  hover:scale-110 duration-400 px-2 py-1 rounded-md  focus:outline-none placeholder:font-extralight' type="number" value={inputvalue} placeholder='amout please.. ' onChange={(e)=>setInputvalue(e.target.value)} />
           <button className=" ml-2 px-5 py-2  bg-black text-white text-sm rounded-md font-semibold hover:shadow-lg hover:bg-black/[0.8] " type="submit">Add</button>
@@ -83,6 +87,7 @@ export default function Content({}) {
         <span  ref={spanRef} className='transition ease-in-out delay-150 hover:-translate-y-1  hover:scale-110  hover:bg-green-600 cursor-pointer  font-mono  bg-green-500 font-semibold text-black px-3 py-2 rounded-md'>Reaming Balance:{NewTotalValue}</span>
       </div>
   </div>
+
   
     </>
   )
