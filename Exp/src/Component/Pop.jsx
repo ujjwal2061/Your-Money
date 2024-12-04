ec202c09eab5f81d0b315cd348fe15bfe7d17ad5
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-05 00:29:08 +0530

    Reaming to stytle it

--- Exp/src/Component/Pop.jsx
+++ Exp/src/Component/Pop.jsx
@@ -1,9 +1,11 @@
 import React   from 'react'
 
-export default function Pop({value}) { 
+export default function Pop({Show,setShowPop}) { 
+  if (!Show) return null ;
   return (
-    <div  className='p-2 rounded-md   bg-red-200  w-1/2 flex flex-col  justify-start md:fex-row '>
-       <span className='p-1 text-sm   font-serif font-semibold  text-black'>Value can't  be zero:{value} </span>
+    <div  className='  fixed  inset-0  bg-opacity-25 backdrop-blur-sm  rounded-md   bg-red-200  w-1/2 flex flex-col  justify-start md:fex-row '>
+       <p className='p-1 text-sm   font-serif font-semibold  text-black'></p>
+       <button onClick={()=>setShowPop(false)}>X</button>
     </div>
   )
 }
