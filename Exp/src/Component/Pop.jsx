1fa8f59b0448ef07ecb44191e258f9bcf1964172
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-05 16:40:34 +0530

    Done with Popup

--- Exp/src/Component/Pop.jsx
+++ Exp/src/Component/Pop.jsx
@@ -1,11 +1,16 @@
 import React   from 'react'
 
-export default function Pop({Show,setShowPop}) { 
-  if (!Show) return null ;
+export default function Pop({ Show, setShowPop }) {
+  if (!Show) return null;
+
   return (
-    <div  className='  fixed  inset-0  bg-opacity-25 backdrop-blur-sm  rounded-md   bg-red-200  w-1/2 flex flex-col  justify-start md:fex-row '>
-       <p className='p-1 text-sm   font-serif font-semibold  text-black'></p>
-       <button onClick={()=>setShowPop(false)}>X</button>
+    <div className='fixed top-0 left-1/2 transform -translate-x-1/2 bg-opacity-25 backdrop-blur-md w-48 rounded-lg bg-white shadow-lg flex flex-row justify-between items-center p-4 sm:w-64 md:w-72 lg:w-80 xl:w-96'>
+      <p className='text-sm font-serif font-semibold text-black sm:text-base md:text-lg'>
+        Value Can't be zero
+      </p>
+      <button onClick={() => setShowPop(false)} className='text-gray-700 hover:text-gray-900 text-lg'>
+        <i className="fa-solid fa-xmark"></i>
+      </button>
     </div>
-  )
-}
+  );
+}  
