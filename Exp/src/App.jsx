5ccdb5730ddd5cac7c40bfcd04319b46c37a46a8
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-06 00:26:33 +0530

    error

--- Exp/src/App.jsx
+++ Exp/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import Selection from './Section/Slection'
 import Header from './Component/Header'
 import Content from './Component/Content'
 import Footer from './Component/Footer'
 import './index.css';
+import { ToogleProvider } from './Section/Toggle'
 export default function App() {
   return (
     <>
-    <Selection>
-      <Header />
-      <Content />
-      <Footer />
-    </Selection>
+    <ToogleProvider>
+     <Selection>
+       <Header />
+       <Content />
+       <Footer />
+     </Selection>
+    </ToogleProvider>
 
     </>
   )
