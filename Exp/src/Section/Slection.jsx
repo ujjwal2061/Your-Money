4f3d965fa3d0e194daa6da25fd60f45d2a484b81
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-06 00:25:22 +0530

    iusses

--- Exp/src/Section/Slection.jsx
+++ Exp/src/Section/Slection.jsx
@@ -1,9 +1,10 @@
-import React from "react"
-
-
+import React  from "react"
+import ToogleContext  from "./Toggle";
 const Selection=({children})=>{
+    const {toggle ,setToogle} =useState(ToogleContext)
 return (
     <section className="relative w-full md:w-[850px] h-[100vh]    mx-auto">
+        <button onClick={()=>setToogle(toggle ==="white"?"black":"white")}>X</button>
             {children}
      </section>
 )
